677503d3bc6666a7a885ea555ee6443dee8444e2
SebSoborg <95217618+SebSoborg@users.noreply.github.com>
2023-12-15 05:47:01 +0100

    Add files via upload

--- Eksamen-main/script.js
+++ Eksamen-main/script.js
@@ -8,6 +8,20 @@ function burger() {
   
 
   }
+  var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    } 
+  });
+}
   var rangeSlider = function() {
     var sliders = document.querySelectorAll('.slidecontainer');
     
